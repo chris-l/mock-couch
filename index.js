96e1259f6916a011ec6c4283a280238fe3c0c49f
Christopher Luna <dev@christopher-luna.com>
2014-04-05 15:25:26 -0600

    The documents are now correctly sorted

--- index.js
+++ index.js
@@ -37,16 +37,25 @@ function mockCouch () {
     var db = self.databases[req.params.db];
 
     // It creates and format the rows
-    var rows = __.pairs(db).map(function(data) {
+    var rows = __(db).chain().pairs(db).sortBy(function(i) {
+      return i[0];
+    }).map(function(data) {
       var id = data[0], doc = data[1];
+
+      // Show the document's content if include_docs=true
       var document = req.query.include_docs === 'true' ? doc : undefined;
+
       return {
         _id : id,
         key : id,
         value : { rev : doc._rev },
         doc : document
       };
-    });
+    }).value();
+
+    // Reverse the order if descending=true
+    rows = req.query.descending === 'true' ? rows.reverse() : rows;
+
     res.send(200, {
       total_rows: Object.keys(db).length,
       offset: 0,
