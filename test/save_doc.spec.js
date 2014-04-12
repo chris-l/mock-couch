f191e03832c797a2e39940a9c25f8bb9a694f9e1
Christopher Luna <dev@christopher-luna.com>
2014-04-12 18:47:22 -0500

    Unit tests for the function handling the PUT and POST requests

---
+++ test/save_doc.spec.js
@@ -0,0 +1,58 @@
+/* jslint node: true */
+/* global describe, it, expect, beforeEach, afterEach */
+'use strict';
+var addDB = require('../lib/addDB'),
+    save_doc_fn = require('../lib/save_doc');
+
+describe('save_doc', function() {
+  var mock_mock, save_doc;
+
+  var dummy_function = function() { };
+  var res = { send : dummy_function, setHeader : dummy_function };
+
+  beforeEach(function() {
+   var db = {
+     people : {
+       miko : {
+         _rev : '12345',
+         name : 'reimu',
+         lastname : 'hakurei'
+       }
+     }
+   };
+   mock_mock = { emit : dummy_function, databases :  db };
+   save_doc = save_doc_fn(mock_mock);
+  });
+
+  it('should allow to create a new document', function() {
+    save_doc.call(mock_mock, { params : { db : 'people', doc : 'player2' }, body : { name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
+    expect(!!mock_mock.databases.people.player2).toBe(true);
+    expect(mock_mock.databases.people.player2.name).toBe('sanae');
+  });
+
+  it('should NOT allow to create a document with the same name of an existing one', function() {
+    save_doc.call(mock_mock, { params : { db : 'people', doc : 'miko' }, body : { name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
+    expect(mock_mock.databases.people.miko.name).toBe('reimu');
+  });
+
+  it('should allow to update a document, requiring to pass the current _rev', function() {
+    save_doc.call(mock_mock, { params : { db : 'people', doc : 'miko' }, body : { _rev : '12345', name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
+    expect(mock_mock.databases.people.miko.name).toBe('sanae');
+  });
+
+  it('should NOT allow to update a document, if the _rev is wrong', function() {
+    save_doc.call(mock_mock, { params : { db : 'people', doc : 'miko' }, body : { _rev : '99999', name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
+    expect(mock_mock.databases.people.miko.name).toBe('reimu');
+
+    save_doc.call(mock_mock, { params : { db : 'people', doc : 'miko' }, body : { _rev : null, name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
+    expect(mock_mock.databases.people.miko.name).toBe('reimu');
+
+    save_doc.call(mock_mock, { params : { db : 'people', doc : 'miko' }, body : { _rev : '', name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
+    expect(mock_mock.databases.people.miko.name).toBe('reimu');
+
+    save_doc.call(mock_mock, { params : { db : 'people', doc : 'miko' }, body : { _rev : '123456', name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
+    expect(mock_mock.databases.people.miko.name).toBe('reimu');
+  });
+
+});
+
