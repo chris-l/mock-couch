cffa7faa88b48cf4f6c6ef13d7888a958bc21c70
Christopher Luna <dev@christopher-luna.com>
2014-05-11 01:31:25 -0500

    Fixed: when a document is created or updated, emits the correct "POST" or "PUT" event.

--- test/save_doc.spec.js
+++ test/save_doc.spec.js
@@ -25,45 +25,45 @@ describe('save_doc', function() {
   });
 
   it('should allow to create a new document', function() {
-    save_doc({ params : { db : 'people', doc : 'player2' }, body : { name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
+    save_doc({ route : { method : 'POST' }, params : { db : 'people', doc : 'player2' }, body : { name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
     expect(!!mock_mock.databases.people.player2).toBe(true);
     expect(mock_mock.databases.people.player2.name).toBe('sanae');
   });
 
   it('should NOT allow to create a document with the same name of an existing one', function() {
-    save_doc({ params : { db : 'people', doc : 'miko' }, body : { name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
+    save_doc({ route : { method : 'POST' }, params : { db : 'people', doc : 'miko' }, body : { name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
     expect(mock_mock.databases.people.miko.name).toBe('reimu');
   });
 
   it('should allow to update a document, requiring to pass the current _rev', function() {
-    save_doc({ params : { db : 'people', doc : 'miko' }, body : { _rev : '12345', name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
+    save_doc({ route : { method : 'POST' }, params : { db : 'people', doc : 'miko' }, body : { _rev : '12345', name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
     expect(mock_mock.databases.people.miko.name).toBe('sanae');
   });
 
   it('should NOT allow to update a document, if the _rev is wrong', function() {
-    save_doc({ params : { db : 'people', doc : 'miko' }, body : { _rev : '99999', name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
+    save_doc({ route : { method : 'POST' }, params : { db : 'people', doc : 'miko' }, body : { _rev : '99999', name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
     expect(mock_mock.databases.people.miko.name).toBe('reimu');
 
-    save_doc({ params : { db : 'people', doc : 'miko' }, body : { _rev : null, name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
+    save_doc({ route : { method : 'POST' }, params : { db : 'people', doc : 'miko' }, body : { _rev : null, name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
     expect(mock_mock.databases.people.miko.name).toBe('reimu');
 
-    save_doc({ params : { db : 'people', doc : 'miko' }, body : { _rev : '', name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
+    save_doc({ route : { method : 'POST' }, params : { db : 'people', doc : 'miko' }, body : { _rev : '', name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
     expect(mock_mock.databases.people.miko.name).toBe('reimu');
 
-    save_doc({ params : { db : 'people', doc : 'miko' }, body : { _rev : '123456', name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
+    save_doc({ route : { method : 'POST' }, params : { db : 'people', doc : 'miko' }, body : { _rev : '123456', name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
     expect(mock_mock.databases.people.miko.name).toBe('reimu');
   });
 
   it('must return the id of the document it just created', function() {
-    save_doc({ params : { db : 'people' }, body : { name : 'cirno' } }, res, dummy_function);
+    save_doc({ route : { method : 'POST' }, params : { db : 'people' }, body : { name : 'cirno' } }, res, dummy_function);
     expect(/^\w+$/.test(result.id)).toBe(true);
 
-    save_doc({ params : { db : 'people', doc : 'player2' }, body : { name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
+    save_doc({ route : { method : 'POST' }, params : { db : 'people', doc : 'player2' }, body : { name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
     expect(result.id).toBe('player2');
   });
 
   it('must return the id of the document it just updated', function() {
-    save_doc({ params : { db : 'people', doc : 'miko' }, body : { _rev : '12345', name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
+    save_doc({ route : { method : 'POST' }, params : { db : 'people', doc : 'miko' }, body : { _rev : '12345', name : 'sanae', lastname : 'kochiya' } }, res, dummy_function);
     expect(result.id).toBe('miko');
   });
 });
