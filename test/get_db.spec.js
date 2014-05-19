a31e82fc4ae71c86d95d293dde288a832f3ddb79
David Wood <bitprobe@gmail.com>
2014-05-19 09:46:26 -0500

    Fixed doc_count on database GET, added tests

---
+++ test/get_db.spec.js
@@ -0,0 +1,50 @@
+/* jslint node: true */
+/*global describe, it, expect, beforeEach, afterEach */
+'use strict';
+var get_db_fn = require('../lib/get_db');
+
+describe('get_db', function() {
+  var mock_mock, get, statusCode, result;
+
+  var dummy_function = function() { };
+  var res = { send : function(status, obj) { statusCode = status; result = obj; }, setHeader : dummy_function };
+
+  beforeEach(function() {
+   var db = {
+     people : {
+       miko : {
+         _rev : '12345',
+         name : 'reimu',
+         lastname : 'hakurei'
+       },
+       magician : {
+         _rev : '67890',
+         name : 'marisa',
+         lastname : 'kirisame'
+       },
+       player2 : {
+         _rev : '334455',
+         name : 'sanae',
+         lastname : 'kochiya'
+       }
+     }
+   };
+   mock_mock = { emit : dummy_function, databases :  db };
+   get = get_db_fn(mock_mock);
+  });
+
+  it('should get any existing database', function() {
+    get({ params : { db : 'people' } }, res, dummy_function);
+    expect(statusCode).toBe(200);
+    expect(result.db_name).toBe('people');
+    expect(result.doc_count).toBe(3);
+  });
+
+  it('should return an error if the database does not exist', function() {
+    get({ params : { db : 'nofound' } }, res, dummy_function);
+    expect(statusCode).toBe(404);
+    expect(result.error).toBe('not_found');
+    expect(result.reason).toBe('no_db_file');
+  });
+
+});
