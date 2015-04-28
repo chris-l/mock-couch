chris-l/mock-couch

commit 8486e7b922b68a724e8d8adb86cc4a53c69aa670

Support for linked document in views
If the value for the _id key in the emitted value is different from the
doc._id being evaluated, attach that document as the result.doc instead.

* Fixes #13
* http://wiki.apache.org/couchdb/Introduction_to_CouchDB_views#Linked_documents

lib/get_view.js
@@ -40,7 +40,11 @@ module.exports = function (self) {
         if (!/^_design/.test(doc._id)) {
           var result = { id : doc._id, key : key, value : value };
           if (!useReduce && includeDocs) {
-            result.doc = doc;
+            if ((typeof value._id === 'string') && (value._id !== doc._id)) {
+              result.doc = self.databases[req.params.db][value._id];
+            } else {
+              result.doc = doc;
+            }
           }
           rows.push(result);
         }

commit 5872745fd2a2f6d19b37cca35210ffc0a2b56377

Add test for emitting null values with included docs
* Test for regression in 8486e7b, issue #21

test/views.spec.js
@@ -52,6 +52,11 @@ describe('views', function () {
                   return a + b.id;
                 }, '');
               }
+            },
+            nullValueView: {
+              map: function (doc) {
+                emit(doc._id, null);
+              }
             }
           },
           _rev : '88888'
@@ -108,5 +113,10 @@ describe('views', function () {
     get({ route : { method : 'GET' }, params : { db : 'people', doc : 'designer', name : 'someview' }, query : { group : 'true', key : '["qball"]' } }, res, dummy_function);
     expect(result.rows.length).toBe(1);
     expect(result.rows[0].key[0]).toBe('qball');
+  });
+
+  it('should be able to handle emitting null values when including docs', function () {
+    get({ route : { method : 'GET' }, params : { db : 'people', doc : 'designer', name : 'nullValueView' }, query : { include_docs : 'true' } }, res, dummy_function);
+    expect(result.rows.length).toBe(4);
   });
 });