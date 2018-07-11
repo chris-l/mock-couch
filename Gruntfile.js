f6beb70351e046730469197b02a0b31f60501c0a
Christopher Luna <dev@christopher-luna.com>
2018-07-11 16:55:24 -0500

    Updated grunt-jslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,9 @@ module.exports = function (grunt) {
           node   : true,
           nomen  : true,
           regexp : true
+        },
+        options : {
+          edition: 'es5'
         }
       }
     },
